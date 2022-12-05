176705723f5a871d9f59a037fa04195209285297
tzarevandrey <tzarevandrey@mail.ru>
2022-12-05 13:38:00 +0300

    fix: simple project

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -1,24 +1,7 @@
-package com.example.lesson1;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
         int rNum = new Random().nextInt(1, 101);
         System.out.println("Guess the number from 1 to 100");
@@ -51,6 +34,6 @@ public class HelloApplication extends Application {
         System.out.println("Game over, press Enter to exit");
         while (!sc.hasNext()) {}
         sc.close();
-        return;
+        System.exit(0);
     }
 }
--- lesson1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.lesson1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.lesson1 to javafx.fxml;
-    exports com.example.lesson1;
-}
